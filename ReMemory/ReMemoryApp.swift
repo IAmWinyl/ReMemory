20f245867fde9257830faea1a88d4d87e6950fe4
Serra Abak <serra.abak@gmail.com>
2023-05-01 20:36:25 -0700

    Initial Commit

---
+++ ReMemory/ReMemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReMemoryApp.swift
+//  ReMemory
+//
+//  Created by Serra Abak on 5/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ReMemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
