55c54f45f53c885747676a59489b4c0da7958f91
Serra Abak <serra.abak@gmail.com>
2023-05-15 20:42:43 -0700

    Added local push notification

--- ReMemory/Models/Notifications.swift
+++ ReMemory/Models/Notifications.swift
@@ -20,11 +20,13 @@ class Notifications {
             return
         }
         
+        let timeToNotify = Double.random(in: 0...24*60*60)
         let memoryToNotify = memories[Int.random(in: 0..<memories.count)]
+        
         let content = UNMutableNotificationContent()
         content.title = "Remember this?"
         content.sound = UNNotificationSound.default
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeToNotify, repeats: false)
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
         
         try await UNUserNotificationCenter.current().add(request)
